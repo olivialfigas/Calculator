d12b3adbbfbd0f11f1338cfe011d31c073357f07
Olivia L. Figas <43144452+olivialfigas@users.noreply.github.com>
2018-11-16 12:26:33 +0000

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,2 +1,30 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Calculator {
+    public static void main(String args[]){
+        new GUI();
+    }
 }
+
+public class GUI extends JFrame implements ActionListener{
+    private JButton number;
+
+    public GUI(){
+        GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GUI.setTitle("Calculator");
+        GUI.setSize(200,350);
+        GUI.setResizable(false);
+        GUI.setVisible(true);
+
+        number = new JButton("1");
+        GUI.add(number);
+
+    }
+public void actionPerformed(ActionEvent e){
+
+}
+}
+
+
