fc3ef0661a030682ad95b094d07f4c8687275991
Olivia L. Figas <43144452+olivialfigas@users.noreply.github.com>
2018-11-20 12:08:56 +0000

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -15,10 +15,12 @@ class GUI extends JFrame implements ActionListener{
     JPanel numbers = new JPanel (new GridLayout(4,3)); //creates number panel 0-9
     JPanel operators = new JPanel (new GridLayout(3,2));
 
+    JTextArea result = new JTextArea();
+
     public GUI(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Calculator");
-        setSize(380,400);
+        setSize(380,300);
         setResizable(false);
         setLayout(null);
         setVisible(true);
@@ -30,6 +32,9 @@ class GUI extends JFrame implements ActionListener{
         CreateOperatorsJPanel();
         operators.setBounds(200,100,125,125);
         add(operators);
+
+        result.setBounds(0,0,380,50);
+        add(result);
     }
     public void actionPerformed(ActionEvent e){
 
@@ -37,14 +42,14 @@ class GUI extends JFrame implements ActionListener{
 
     private void CreateNumbersJPanel(){
         for(int i=0;i<number.length;i++){
-            number[i] = new JButton(Integer.toString(i)); //from int to String
-            number[i].addActionListener(this); //pressing the button
+            number[i] = new JButton(Integer.toString(i)); // converts integers 0-9 to String to print them on buttons
             numbers.add(number[i]); //add the buttons 0-9
 
         }
     }
 
     private void CreateOperatorsJPanel(){
+        //creating each button seperately
         operator[0] = new JButton("+");
         operator[1] = new JButton("-");
         operator[2] = new JButton("x");
@@ -52,7 +57,7 @@ class GUI extends JFrame implements ActionListener{
         operator[4] = new JButton("=");
         operator[5] = new JButton("C");
 
-        for(JButton o:operator) //for each loop
+        for(JButton o:operator) //for each loop to add each button from array
             operators.add(o);
     }
 }
